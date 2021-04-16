82f928e4eec312f4334a66668adf9c25205d78f5
Ben Sharon <ben@netninja.me>
2021-04-16 11:00:25 -0400

    fixed aks identity reference

--- main.tf
+++ main.tf
@@ -74,6 +74,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_role_assignment" "role_assignment" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity.principal_id
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
 }
 
