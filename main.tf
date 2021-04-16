02ba86195a548f240c4ab9db214576255d89b0cb
Ben Sharon <ben@netninja.me>
2021-04-16 10:58:13 -0400

    added container registery and role assignment for aks access

--- main.tf
+++ main.tf
@@ -38,10 +38,22 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "${var.application}-${var.environment}-${var.location_code}-rg"
   location = var.location
+
+  tags = local.tags
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                     = "${var.application}-${var.environment}-${var.location_code}-acr"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  sku                      = "Basic"
+  admin_enabled            = false
+
+  tags = local.tags
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "${var.application}-${var.environment}-${var.location_code}-rg"
+  name                = "${var.application}-${var.environment}-${var.location_code}-aks"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "labaks"
@@ -58,3 +70,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   tags = local.tags
 }
+
+resource "azurerm_role_assignment" "role_assignment" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.identity.principal_id
+}
+
