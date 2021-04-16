570a3f6cfd5df8c601a1601708be9c470e2f02c2
Ben Sharon <ben@netninja.me>
2021-04-16 10:31:36 -0400

    added initial varaibles, provider, group, and aks cluster

---
+++ main.tf
@@ -0,0 +1,60 @@
+variable "location" {
+  type = string
+}
+
+variable "location_code" {
+  type = string
+}
+
+variable "application" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+locals {
+  tags = {
+    "application" = var.application
+    "environment" = var.environment
+  }
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.56.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "5be7a915-38a1-4956-8055-13b1d13d9018"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.application}-${var.environment}-${var.location_code}"
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "rg-${var.application}-${var.environment}-${var.location_code}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "labaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.tags
+}
